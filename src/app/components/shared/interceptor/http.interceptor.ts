cdcd1a7f4ce5043069072edf0462a4ea77902823
Kunszt Norbert <kunszt.norbert@gmail.com>
2018-07-01 12:23:32 +0200

    test http interceptor header

--- src/app/components/shared/interceptor/http.interceptor.ts
+++ src/app/components/shared/interceptor/http.interceptor.ts
@@ -16,8 +16,17 @@ export class AuthHttpInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>,
               next: HttpHandler): Observable<HttpEvent<any>> {
 
-        req = req.clone({headers: req.headers.set('Content-Type', 'application/json')});
+        req = req.clone({headers: req.headers.set('Content-Type', 'X-Auth-Token')});
         req = req.clone({headers: req.headers.set('Access-Control-Allow-Origin', '*')});
+        req = req.clone({headers: req.headers.set('Access-Control-Allow-Method', 'GET, POST, PATCH, PUT, DELETE, OPTION')});
+        req = req.clone({headers: req.headers.set('Access-Control-Allow-Headers', 'Origin')});
+
+
+        /*
+        Access-Control-Allow-Origin: *
+        Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS
+        Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token
+        */
 
         return next.handle(req);
     }
